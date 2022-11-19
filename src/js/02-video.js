b8f1d4ec6bfc7d347cc839fda9cc90cb41a120fd
TrueMolni <truemolni@gmail.com>
2022-11-19 20:09:25 +0200

    222

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,28 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+//імпорти
+const STORAGE_KEY = 'videoplayer-current-time';
 
+//створюємо відеоплеєр
 iframe = document.querySelector('iframe');
-console.log(iframe);
-
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+//методом оn() відслідковуємо зміну часу в плеєрі, не частіше разу в сек
+player.on('timeupdate', throttle(onPlay, 1000));
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+//зберігаємо в локальне сховище дані з плеєру
+function onPlay(data) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
+
+//підгружаємо дані після оновлення сторінки, якщо вони є
+function getCurrentTime() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  if (savedData) {
+    const parsedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    // відновлюємо відтворення відео з попереднього моменту
+    player.setCurrentTime(parsedData.seconds);
+  }
+}
+// викликаємо функцію, щоб все працювало
+getCurrentTime();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,60 @@
+import throttle from 'lodash.throttle';
+// імпортуємо throttle
 
+// наші посилання на елементи форми
+const refs = {
+  formRef: document.querySelector('.feedback-form'),
+  textareaRef: document.querySelector('textarea'),
+  inputRef: document.querySelector('input'),
+};
+
+// основні змінні
+const formData = {};
+const FORM_KEY = 'user-form-data';
+
+//додаємо слухачів подій для делегування форми
+refs.formRef.addEventListener('submit', onSubmitHandle);
+refs.formRef.addEventListener('input', throttle(onFormHandle, 500));
+
+//підгружаємо дані
+loadFormData();
+
+// відстежуємо івенти форми і записуємо в локальне сховище
+function onFormHandle() {
+  formData.email = refs.inputRef.value;
+  formData.message = refs.textareaRef.value;
+
+  localStorage.setItem(FORM_KEY, JSON.stringify(formData));
+}
+
+// ф-ія для загрузки зі сховища валідного js об'єкту
+function loadFormData() {
+  if (formData) {
+    try {
+      const parsedData = JSON.parse(localStorage.getItem(FORM_KEY));
+
+      const savedMessage = parsedData.message;
+      const savedEmail = parsedData.email;
+
+      if (savedMessage) {
+        refs.textareaRef.value = savedMessage;
+      }
+
+      if (savedEmail) {
+        refs.inputRef.value = savedEmail;
+      }
+    } catch (error) {
+      console.log('Ouch!');
+    }
+  }
+}
+
+// обробник події submit. очищує форму і дані з локального сховища
+function onSubmitHandle(event) {
+  event.preventDefault();
+  const parsedData = JSON.parse(localStorage.getItem(FORM_KEY));
+  console.log('comment: ', parsedData.message);
+  console.log('email: ', parsedData.email);
+  event.currentTarget.reset();
+  localStorage.removeItem(FORM_KEY);
+}
